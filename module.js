39942ecf0527895556259a56768574bc6555b59d
SuzanneJokhu <[sjokhu25@gmail.com]>
2024-04-29 10:14:10 -0400

    changed

--- module.js
+++ module.js
@@ -1,9 +1,11 @@
+const arrow=(num1=5, num2=6) => num1 + num2;
+console.log(arrow);
 //import { name, phone } from "./person.js";
 
-import message from "./person.js";
+//import message from "./person.js";
 //let myMessage  = "My name is "+ name+ " and my Phone no is " + phone;
 
-document.getElementById("myName").innerHTML = message();
+//document.getElementById("myName").innerHTML = message();
 
 //document.getElementById("myPhone").innerHTML = phone;
 
